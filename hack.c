978602a96147599315c094474f700511524d8296
Abdullah-0110 <abdullahdev88@gmail.com>
2023-10-22 23:22:38 +0500

    addition of two number

--- hack.c
+++ hack.c
@@ -6,3 +6,19 @@
 */
 //in this hack nothing for work just setup of github
 
+#include<stdio.h>
+
+int main(void){
+
+    int num1,num2,sum;
+
+    printf("Enter first Number=>");
+
+    scanf("%d", & num1);
+
+    printf("Enter Second Number=>");
+
+    scanf("%d", & num2);
+
+    
+}
